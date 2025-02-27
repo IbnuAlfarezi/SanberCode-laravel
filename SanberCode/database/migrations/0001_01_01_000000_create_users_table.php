be94b46ce2af72e15065db17f36695f8eac0320b
IbnuAlfarezi <muh.ibnufarezi@gmail.com>
2025-02-28 00:29:15 +0800

    Tugas 14 – Mengatur Database dengan Migration

--- SanberCode/database/migrations/0001_01_01_000000_create_users_table.php
+++ SanberCode/database/migrations/0001_01_01_000000_create_users_table.php
@@ -9,14 +9,15 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 255);
+            $table->string('email', 255)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 255);
+            $table->enum('role', ['admin', 'user']);
             $table->rememberToken();
             $table->timestamps();
         });
@@ -37,10 +38,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
         Schema::dropIfExists('password_reset_tokens');
